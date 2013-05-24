e5eadf847b6d9f485f7830319a35600a24c3ae47
segiddins <segiddins@segiddins.me>
2013-05-24 12:08:06 -0300

    Split method headers

--- FDWKit/FDWClient.h
+++ FDWKit/FDWClient.h
@@ -12,7 +12,10 @@
 #pragma mark -
 #pragma mark Authentication
 
-- (void)authenticateEmail:(NSString *)email password:(NSString *)password clientKey:(NSString *)clientKey completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
+- (void)authenticateEmail:(NSString *)email 
+                 password:(NSString *)password 
+                clientKey:(NSString *)clientKey 
+        completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
 
 @property (readonly, strong) NSString *accessToken;
 
@@ -24,30 +27,60 @@
 
 - (void)fetchCurrentSubscriptionsWithCompletionHandler:(void (^)(BOOL success, NSArray *subscriptions, NSError *error))completionHandler;
 
-- (void)subscribeToURL:(NSString *)url completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
+- (void)subscribeToURL:(NSString *)url 
+     completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
 
-- (void)unsubscribeFromFeed:(FDWFeed *)feed completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
+- (void)unsubscribeFromFeed:(FDWFeed *)feed 
+          completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
 
 #pragma mark -
 #pragma mark Feed Items
 
-- (void)fetchFeedItemsWithRead:(NSNumber *)read starred:(NSNumber *)starred feedID:(NSString *)feedID createdSince:(NSDate *)createdSince updatedSince:(NSDate *)updatedSince limit:(NSNumber *)limit offset:(NSNumber *)offset completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
+- (void)fetchFeedItemsWithRead:(NSNumber *)read 
+                       starred:(NSNumber *)starred 
+                        feedID:(NSString *)feedID 
+                  createdSince:(NSDate *)createdSince 
+                  updatedSince:(NSDate *)updatedSince 
+                         limit:(NSNumber *)limit 
+                        offset:(NSNumber *)offset 
+             completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
 
-- (void)fetchFeedItemsWithRead:(NSNumber *)read starred:(NSNumber *)starred feed:(FDWFeed *)feed createdSince:(NSDate *)createdSince updatedSince:(NSDate *)updatedSince limit:(NSNumber *)limit offset:(NSNumber *)offset completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
+- (void)fetchFeedItemsWithRead:(NSNumber *)read 
+                       starred:(NSNumber *)starred 
+                          feed:(FDWFeed *)feed 
+                  createdSince:(NSDate *)createdSince 
+                  updatedSince:(NSDate *)updatedSince 
+                         limit:(NSNumber *)limit 
+                        offset:(NSNumber *)offset 
+             completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
 
-- (void)searchForFeedItemsWithTerm:(NSString *)searchTerm limit:(NSNumber *)limit offset:(NSString *)offset completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
+- (void)searchForFeedItemsWithTerm:(NSString *)searchTerm 
+                             limit:(NSNumber *)limit 
+                            offset:(NSString *)offset 
+                 completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
 
-- (void)updateFeedItem:(FDWItem *)feedItem withRead:(NSNumber *)read starred:(NSNumber *)starred readLater:(NSNumber *)readLater completionHandler:(void (^)(BOOL success, FDWItem *feedItem, NSError *error))completionHandler;
+- (void)updateFeedItem:(FDWItem *)feedItem 
+              withRead:(NSNumber *)read 
+               starred:(NSNumber *)starred
+             readLater:(NSNumber *)readLater
+     completionHandler:(void (^)(BOOL success, FDWItem *feedItem, NSError *error))completionHandler;
 
-- (void)markFeedItemsAsRead:(NSArray *)feedItems feed:(FDWFeed *)feed createdBefore:(NSDate *)createdBefore completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
+- (void)markFeedItemsAsRead:(NSArray *)feedItems 
+                       feed:(FDWFeed *)feed
+              createdBefore:(NSDate *)createdBefore
+          completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
 
 #pragma mark -
 #pragma mark Streams
 
 - (void)fetchCurrentStreamsWithCompletionHandler:(void (^)(BOOL success, NSArray *streams, NSError *error))completionHandler;
 
-- (void)fetchStreamItems:(FDWStream *)stream limit:(NSNumber *)limit offset:(NSNumber *)offset completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
+- (void)fetchStreamItems:(FDWStream *)stream
+                   limit:(NSNumber *)limit
+                  offset:(NSNumber *)offset
+       completionHandler:(void (^)(BOOL success, NSArray *feedItems, NSError *error))completionHandler;
 
-- (void)destroyStream:(FDWStream *)stream completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
+- (void)destroyStream:(FDWStream *)stream
+    completionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
 
 @end
